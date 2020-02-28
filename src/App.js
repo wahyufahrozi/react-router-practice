722ae09b27a14ec3728ad88a0574858f4433c593
wahyufahrozi <wahyufahrozi97@gmail.com>
2020-02-29 02:55:45 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import "./App.css";
+
+const HomePage = props => {
+  console.log("homepage", props);
+
+  return (
+    <div>
+      <Link to="/topics">Topics</Link>
+      <br />
+      <button onClick={() => props.history.push("/topics")}>Topics</button>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+const TopicList = props => {
+  console.log("topicList", props);
+  return (
+    <div>
+      <h1>Topic List Page</h1>
+      <Link to={`${props.match.url}/13`}>To Topic 13</Link>
+      <Link to={`${props.match.url}/21`}>To Topic 21</Link>
+      <Link to={`${props.match.url}/43`}>To Topic 43</Link>
+    </div>
+  );
+};
+
+const TopicDetail = props => {
+  console.log("topcidetail", props);
+  return (
+    <div>
+      <h1>Topic Detail : {props.match.params.topicId} </h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/topics" component={TopicList} />
+      <Route path="/topics/:topicId" component={TopicDetail} />
     </div>
   );
 }
